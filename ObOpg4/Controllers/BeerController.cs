ed0c6fa8b586b96a79596eb99f27a134f40a4845
gstok123 <gstok123@gmail.com>
2021-02-25 08:58:06 +0100

    Add project files.

---
+++ ObOpg4/Controllers/BeerController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ObOpg1;
+using ObOpg4.Managers;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ObOpg4.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BeerController : ControllerBase
+    {
+        private static BeerManager _manager = new BeerManager(new List<Beer>()
+        {
+            new Beer(1,"Carlsberg", 15,5),
+            new Beer(2,"Tuborg", 13.5,5),
+            new Beer(3,"Corona", 20,8),
+            new Beer(4,"Bud Light", 20,3)
+        });
+
+        // GET: api/<BeerController>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<Beer>> Get()
+        {
+            return Ok(_manager.GetAll());
+        }
+
+        // GET api/<BeerController>/5
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<Beer> Get(int id)
+        {
+            Beer beer = _manager.GetById(id);
+            if(beer == null)
+            {
+                return NotFound("Id not found : " + id);
+            }
+            else
+            {
+                return Ok(beer);
+            }
+        }
+
+        // POST api/<BeerController>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<Beer> Post([FromBody] Beer value)
+        {
+            try
+            {
+                Beer b = _manager.Post(value);
+                string uri = Url.RouteUrl(RouteData.Values) + "/" + b.Id;
+                return Created(uri, b);
+            }
+            catch(ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        // PUT api/<BeerController>/5
+        [HttpPut("{id}")]
+        public ActionResult<Beer> Put(int id, [FromBody] Beer value)
+        {
+            try
+            {
+                Beer beer = _manager.Update(id, value);
+                if(beer == null)
+                {
+                    return NotFound("Id not found i data collection: " + id);
+                }
+                else
+                {
+                    return Ok(beer);
+                }
+
+            }
+            catch(ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // DELETE api/<BeerController>/5
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<Beer> Delete(int id)
+        {
+            Beer beer = _manager.Delete(id);
+            if(beer == null)
+            {
+                return NotFound("No such Id: " + id);
+            }
+            else
+            {
+                return Ok(beer);
+            }
+        }
+    }
+}
---
+++ ObOpg4/Managers/BeerManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ObOpg1;
+
+namespace ObOpg4.Managers
+{
+    public class BeerManager
+    {
+        private static List<Beer> Data { get; set; }
+        public BeerManager()
+        {
+            Data = new List<Beer>();
+        }
+        public BeerManager(List<Beer> data)
+        {
+            Data = data;
+        }
+
+        public IEnumerable<Beer> GetAll()
+        {
+            return Data;
+        }
+
+        public Beer GetById(int id)
+        {
+            return Data.Find((b) => b.Id == id);
+        }
+
+        public Beer Post(Beer value)
+        {
+            Data.Add(value);
+            return value;
+        }
+
+        public Beer Update(int id, Beer value)
+        {
+            int index = Data.FindIndex((e) => e.Id == value.Id);
+            if (index != -1)
+            {
+                Data[index] = value;
+                return Data[index];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public Beer Delete(int id)
+        {
+            Beer beer = GetById(id);
+            Data.Remove(beer);
+            return beer;
+        }
+    }
+}
